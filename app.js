003bd0a46246e433d9f4c6e4a0b7ea3f930e8db0
Hernan Di Tano <hditano@outlook.com>
2021-01-14 04:54:27 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// const dia = "Lunes"
+
+// let usuario = prompt("Que dia es?")
+
+// if (dia === usuario) {
+//   console.log(`el dia es ${dia}`)
+// } else {
+//   console.log("El dia es otro")
+// }
+
+// const edad = 66;
+
+// if (edad > 5 && edad < 65) {
+//   console.log("La edad es mayor a 5 y menor que 65")
+// } else {
+//   console.log("la edad es menor a 5 o mayor a 65")
+// }
+
+let myArray = ['Hernan', 'Sebastian', 'Gimena']
+
+// myArray.push('Zoe');
+
+// console.log(myArray)
+
+// myArray.pop();
+
+// console.log(myArray)
+
+// myArray.shift();
+// console.log(myArray);
+
+// myArray.unshift('Run');
+
+// console.log(myArray);
+
+
+let myArray2 = myArray.slice(1);
+console.log(myArray2);
+
+
