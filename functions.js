2ab243a55677c6e3ec35d54a856c948de2dc0b09
Hernan Di Tano <hditano@outlook.com>
2021-01-18 06:40:55 -0300

    Function Exercises

---
+++ functions.js
@@ -0,0 +1,14 @@
+//**** Functions *****/
+
+
+// printHeart() {
+//   console.log("<3");
+// }
+
+// printHeart()
+
+function rant(message) {
+  console.log(message.toUpperCase());
+}
+
+rant('i hate bees')
