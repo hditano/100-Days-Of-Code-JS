c8513106743b6ffe0a7db78b3a87e60bd7907eb6
Hernan Di Tano <hditano@gmail.com>
2021-01-20 15:29:10 -0300

    Function Expressions

--- functions.js
+++ functions.js
@@ -74,19 +74,27 @@
 //   }
 // }
 
-function returnDay(number) {
-  if (number < 1 || number > 7) {
-    return null;
-  } else {
-    const myDaysObject = {
-      1 : 'Monday',
-      2 : 'Tuesday',
-      3 : 'Wednesday',
-      4 : 'Thursday', 
-      5 : 'Friday', 
-      6 : 'Saturday', 
-      7 : 'Sunday',
-    }
-    return myDaysObject[number];
-  }
-}
+// function returnDay(number) {
+//   if (number < 1 || number > 7) {
+//     return null;
+//   } else {
+//     const myDaysObject = {
+//       1 : 'Monday',
+//       2 : 'Tuesday',
+//       3 : 'Wednesday',
+//       4 : 'Thursday', 
+//       5 : 'Friday', 
+//       6 : 'Saturday', 
+//       7 : 'Sunday',
+//     }
+//     return myDaysObject[number];
+//   }
+// }
+
+/** Function Expressions  */
+
+// const pi = function (num) {
+//   console.log(`PI es ${num}`)
+// }
+
+// pi(123);
