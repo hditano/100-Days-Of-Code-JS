01775715b45349a963e0c3b4b554a599c5529ea4
Hernan Di Tano <hditano@gmail.com>
2021-01-29 18:20:40 -0300

    InnerText/innerHTML/textContent

---
+++ app.js
@@ -0,0 +1,22 @@
+/* getElementByID */
+
+// const image = document.getElementById('unicorn');
+// const heading = document.getElementById('mainheading');
+
+/* getElementByTagName */
+
+// const allImages = document.getElementsByTagName('img')
+
+// for (let images of allImages) {
+//   console.log(images.src);
+// }
+
+/* querySelector Exercise 52 */
+
+// const doneTodos = document.querySelectorAll('.done');
+// const checkbox = document.querySelector('#scales');
+
+/* InnerText/textContent/innerHTML */
+
+// const newText = document.querySelector('span').textContent = 'Disgusting';
+
