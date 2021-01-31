817b19d171f9884e3bd2b62c50d15501c8c32942
Hernan Di Tano <hditano@gmail.com>
2021-01-31 15:27:40 -0300

    Exercises Filter

--- app.js
+++ app.js
@@ -20,3 +20,46 @@
 
 // const newText = document.querySelector('span').textContent = 'Disgusting';
 
+/* Attributes Exercise 54 */
+
+// const newImage = document.querySelector('#egg');
+// newImage.setAttribute('src', 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg');
+// newImage.setAttribute('alt', 'chicken');
+
+/* Changing Style Exercise 55 */
+
+// const newStyle = document.querySelector('#container');
+// newStyle.style.textAlign = 'center';
+
+// const newImage = document.querySelector('img');
+// newImage.style.width = '150px';
+// newImage.style.borderRadius = '50%';
+
+/* Changing Syle Exercise 56 */
+
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+// const span = document.querySelectorAll('span');
+
+// for (let i = 0; i < span.length; i++) { 
+//     span[i].style.color = colors[i];
+// }
+
+// const arrayNames = ['Hernan', 'Zoe', 'Run']
+
+// const result = arrayNames.map(item => item.length);
+
+// const final = result.reduce((a,b) => a + b, 0);
+
+// console.log(final)
+
+/* Filter Exercise */
+
+// let users = [
+//   {id: 1, name: 'Hernan'},
+//   {id: 2, name: 'Admin'},
+// ]
+
+// let newUsers = users.filter(item => item.id < 3);
+
+// console.log(newUsers);
